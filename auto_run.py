53ed70f2c2ddbc8be92bd5e7272b3f483e2a1198
zhengchu <zhengchu@zhejianglab.com>
2023-05-31 11:02:41 +0800

    cpu_threads_num_list

--- auto_run.py
+++ auto_run.py
@@ -5,50 +5,50 @@ import threading
 import os, getopt
 
 
-# python auto_run.py -b {branch} -p {parallel_num} -l {loop_num} -c {cpu_threads_num}
+# python auto_run.py -b {branch} -p {parallel_num} -c {cpu_threads_num_list}
 branch = ""
-parallel_num, loop_num, cpu_threads_num = 0, 0, 0
-opts,args = getopt.getopt(sys.argv[1:],'-b:-p:-l:-c:')
+parallel_num = 0
+cpu_threads_num_list = []
+opts,args = getopt.getopt(sys.argv[1:],'-b:-p:-c:')
 for opt_name,opt_value in opts:
     if opt_name in ('-b'):
         branch = opt_value
     if opt_name in ('-p'):
         parallel_num = int(opt_value)
-    if opt_name in ('-l'):
-        loop_num = int(opt_value)
     if opt_name in ('-c'):
-        cpu_threads_num = opt_value
+        cpu_threads_num_list = list(map(int, opt_value.split(" ")))
 
-filename = "{}_{}_{}_{}.txt".format(branch, parallel_num, loop_num, cpu_threads_num)
-print(filename)
-f = open("./data/" + filename, "a+")
+print("{} {} {}", branch, parallel_num, cpu_threads_num_list)
 
 os.system("git switch " + branch)
 os.system("make")
 
 
-def do_loop():
-    for j in range(loop_num):
-        time1 = time.time()
-        result = subprocess.run(["./hybrid2", cpu_threads_num], stdout=subprocess.PIPE)
-        time2 = time.time()
-        w = "{} {}".format(j, (time2 - time1))
-        print(w)
-        f.write(w + "\n")
+def do_loop(cpu_threads_num):
+    with open("./data/{}_{}_{}.txt".format(branch, parallel_num, cpu_threads_num), "a+") as f:
+        for j in range(20):
+            time1 = time.time()
+            result = subprocess.run(["./hybrid2", str(cpu_threads_num)], stdout=subprocess.PIPE)
+            # result = subprocess.run(["ls"], stdout=subprocess.PIPE)
+            time2 = time.time()
+            w = "{} {}".format(j, (time2 - time1))
+            print(w)
+            print("write to {}".format(f))
+            f.write(w + "\n")
 
+for cpu_threads_num in cpu_threads_num_list: 
+    begin = time.time()
+    t = []
+    for i in range(parallel_num):
+        ti = threading.Thread(target=do_loop, args=(cpu_threads_num, ))
+        t.append(ti)
+        ti.start()
 
-# print(result.stdout.decode())
-begin = time.time()
-t = []
-for i in range(parallel_num):
-    ti = threading.Thread(target=do_loop)
-    t.append(ti)
-    ti.start()
+    for i in range(parallel_num):
+        t[i].join()
 
-for i in range(parallel_num):
-    t[i].join()
-
-end = time.time()
-s = "total {}".format((end - begin))
-print(s)
-f.write(s + "\n")
+    end = time.time()
+    s = "total {}".format((end - begin))
+    print(s)
+    with open("./data/{}_{}_{}.txt".format(branch, parallel_num, cpu_threads_num), "a+") as f:
+        f.write(s + "\n")
