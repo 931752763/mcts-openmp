f16cbf7e4dfdafaea73432e32dd51906018e78f4
zhengchu <931752763@qq.com>
2023-06-02 15:04:16 +0800

    mcts_cuda.cu conflict

--- auto_run.py
+++ auto_run.py
@@ -5,32 +5,15 @@ import threading
 import os, getopt
 from openpyxl import Workbook, load_workbook
 
-# python auto_run.py -b {branch} -p {parallel_num} -c {cpu_threads_num_list}
-branch = ""
-parallel_num = 0
-cpu_threads_num_list = []
-opts,args = getopt.getopt(sys.argv[1:],'-b:-p:-c:')
-for opt_name,opt_value in opts:
-    if opt_name in ('-b'):
-        branch = opt_value
-    if opt_name in ('-p'):
-        parallel_num = int(opt_value)
-    if opt_name in ('-c'):
-        cpu_threads_num_list = list(map(int, opt_value.split(" ")))
 
-os.system("git checkout " + branch)
-os.system("make")
-print("parallel_num: {}, cpu_threads_num_list: {}".format(parallel_num, cpu_threads_num_list))
-data_txt = "../data/{}_{}_{}.xlsx"
-
-def do_loop(cpu_threads_num):
-    print("mcts cpu_threads_num: {}, mcts will execute 10 times in sequence".format(cpu_threads_num))
+def do_loop(cpu_threads_num, parallel_num):
+    print("mcts cpu_threads_num: {}, parallel_num: {}, mcts will execute 10 times in sequence".format(cpu_threads_num, parallel_num))
     file = data_txt.format(branch, parallel_num, cpu_threads_num)
     for j in range(10):
         time1 = time.time()
-        process = subprocess.Popen(["./hybrid2", str(cpu_threads_num)], stdout=subprocess.PIPE)
-        process.wait()
-        # result = subprocess.run(["ls"], stdout=subprocess.PIPE)
+        # process = subprocess.Popen(["./hybrid2", str(cpu_threads_num)], stdout=subprocess.PIPE)
+        # process.wait()
+        result = subprocess.run(["ls"], stdout=subprocess.PIPE)
         time2 = time.time()
         new_row = [process.pid, (time2 - time1)]
         print(new_row)
@@ -40,38 +23,59 @@ def do_loop(cpu_threads_num):
             ws.append(new_row)
             wb.save(file)
 
-for cpu_threads_num in cpu_threads_num_list:
-    if branch == "openmp":
-        omp_num_threads = cpu_threads_num + 2
-        subprocess.run(["export", "OMP_NUM_THREADS={}".format(omp_num_threads)], stdout=subprocess.PIPE)
-        print("openmp OMP_NUM_THREADS:")
-        subprocess.run(["echo", "$OMP_NUM_THREADS"], stdout=subprocess.PIPE)
-    wb = Workbook()
-    ws = wb.active
-    file = data_txt.format(branch, parallel_num, cpu_threads_num)
-    title = ["thread_id", "time (s)"]
-    ws.append(title)
-    wb.save(file)
-    begin = time.time()
-    begin_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(begin))
-    t = []
-    lock = threading.Lock()
-    for i in range(parallel_num):
-        ti = threading.Thread(target=do_loop, args=(cpu_threads_num, ))
-        t.append(ti)
-        ti.start()
 
-    for i in range(parallel_num):
-        t[i].join()
+# python auto_run.py -b {branch} -p {parallel_num} -c {cpu_threads_num_list}
+branch_list = []
+parallel_num_list = []
+cpu_threads_num_list = []
+opts,args = getopt.getopt(sys.argv[1:],'-b:-p:-c:')
+for opt_name,opt_value in opts:
+    if opt_name in ('-b'):
+        branch_list = opt_value.split(" ")
+    if opt_name in ('-p'):
+        parallel_num_list = list(map(int, opt_value.split(" ")))
+    if opt_name in ('-c'):
+        cpu_threads_num_list = list(map(int, opt_value.split(" ")))
 
-    end = time.time()
-    end_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end))
-    with lock:
-        wb = load_workbook(file)
-        ws = wb.active
-        ws.append([begin_str, end_str])
-        wb.save(file)
-    s = "total {}".format((end - begin))
-    print(s)
-    print("sleep 5 min to seperate")
-    time.sleep(300)
+
+for branch in branch_list:
+    os.system("git checkout " + branch)
+    os.system("make")
+    print("parallel_num_list: {}, cpu_threads_num_list: {}".format(parallel_num_list, cpu_threads_num_list))
+    data_txt = "../data/{}_{}_{}.xlsx"
+
+    for cpu_threads_num in cpu_threads_num_list:
+        for parallel_num in parallel_num_list:
+            if branch == "openmp":
+                omp_num_threads = cpu_threads_num + 2
+                os.environ['OMP_NUM_THREADS'] = str(omp_num_threads)
+                print("openmp OMP_NUM_THREADS = {}".format(omp_num_threads))
+            wb = Workbook()
+            ws = wb.active
+            file = data_txt.format(branch, parallel_num, cpu_threads_num)
+            title = ["thread_id", "time (s)"]
+            ws.append(title)
+            wb.save(file)
+            begin = time.time()
+            begin_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(begin))
+            t = []
+            lock = threading.Lock()
+            for i in range(parallel_num):
+                ti = threading.Thread(target=do_loop, args=(cpu_threads_num, parallel_num, ))
+                t.append(ti)
+                ti.start()
+
+            for i in range(parallel_num):
+                t[i].join()
+
+            end = time.time()
+            end_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end))
+            with lock:
+                wb = load_workbook(file)
+                ws = wb.active
+                ws.append([begin_str, end_str])
+                wb.save(file)
+            s = "total {}".format((end - begin))
+            print(s)
+            print("sleep 5 min to seperate")
+            time.sleep(300)
